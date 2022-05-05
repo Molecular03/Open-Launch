0b1c915727c1f607089bd9279383f272626b13fe
Molecular03 <evlapensee06@gmail.com>
2022-05-04 20:15:29 -0400

    Updated pressure error throw

--- Quick-Version-Updates/v3.py
+++ Quick-Version-Updates/v3.py
@@ -9,19 +9,21 @@ from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,NavigationToolb
 
 def set_window():
     window = tk.Tk()
-    window.columnconfigure(1,weight=2) #Tells frame to expand to fill extra space if window is resized and where to expand to.
+    window.columnconfigure(2,weight=2) #Tells frame to expand to fill extra space if window is resized and where to expand to.
     window.rowconfigure(0, weight=1)
+    plotFrame = Frame(window)
+    plotFrame.grid(column=1,row=0)
     inputSection = Frame(window)
-    inputSection.grid(column=1)
-    return window, inputSection
+    inputSection.grid(column=2,row=0)
+    return window, inputSection, plotFrame
 
-def integrate_plot(window):
+def integrate_plot():
     global canvas
     fig = plt.figure(figsize=(7,9)) #Defines the matplotlib figure
 	#Create tkinter canvas holding the matplotlib Figure
-    canvas = FigureCanvasTkAgg(fig, master=window)
-    canvas.get_tk_widget().grid(row=0,column=0) #places canvas on window
-    toolbarFrame = tk.Frame(master=window)
+    canvas = FigureCanvasTkAgg(fig, master=plotFrame)
+    canvas.get_tk_widget().grid(row=0,column=0) #places canvas on plotFrame
+    toolbarFrame = tk.Frame(master=plotFrame)
     toolbarFrame.grid(row=1,column=0)
     toolbar = NavigationToolbar2Tk(canvas,toolbarFrame)
 
@@ -35,12 +37,13 @@ def add_plot_values():
     canvas.draw()
 	
 def universal_constants():
+    global constants
+    constants = []
     tUnit = tUnitSelection.get()
     #Define G, Mass of earth (MoE), molar mass of dry air (MolAir), molar gass constant (R), P0, T0, rho0, g0
     G, g0 = 6.67e-11, 9.81 #Gravitational constant and Gravitational acceleration --> sea level
     MoE = 5.972e24
     molAir, R = 0.028964, 8.3145
-    #P0 = float(input('Air pressure in launch area (Pa): ')) #Possible addition --> Add in averages for certain regions of the world by time of year
     error_string = ''
     getTemp = str(TempEntry.get()) 
     if getTemp.isnumeric() is False: 
@@ -54,13 +57,21 @@ def universal_constants():
         T0 += 273.15 #Initial temperature in kelvin
     elif tUnit == 'F':
         T0 = ((T0 - 32) * 5/9) + 273.15 #Initial temperature in kelvin
+        error_string += '\nPlease consider using celsius or kelvin next time'
     else:
         error_string += '\nPlease select a Unit of temperature'
+    if PressureEntry.get().isnumeric() is False:
+        error_string += '\nPlease enter the pressure at the pad'
+        P0 = 1.0
+    else: 
+        P0 = float(PressureEntry.get())
     errorLabel.config(text=error_string)
     inputSection.update()
-    #rho0 = (molAir*P0)/(R*T0)  #Air density --> sea level
-    #constants = [G, MoE, molAir, R, P0, T0, rho0, g0]
-    #return constants
+    passableErrors = ['','\nPlease consider using celsius or kelvin next time','\nThank you for using kelvin']
+    if error_string in passableErrors:
+        rho0 = (molAir*P0)/(R*T0)  #Air density --> sea level
+        constants = [G, MoE, molAir, R, P0, T0, rho0, g0]
+        return constants
 
 def rocket_specs():
     #Get rockets specs: thrust, drag coef., diameter, area, wet + dry mass, Isp, mass flow rate
@@ -81,10 +92,10 @@ def euler_cromer():
     #Plot thrust, drag force, and g vs. t (Optional)
     pass
 
-window, inputSection = set_window()
-integrate_plot(window)
-Button(window, text='Plot', command=add_plot_values).grid(row=0,column=2) #Adds values to the plot
-Button(window, text='Quit', command=quit).grid(row=1,column=2)
+window, inputSection, plotFrame = set_window()
+integrate_plot()
+plotButton = Button(inputSection, text='Plot', command=add_plot_values).grid(row=4,column=1) #Adds values to the plot
+quitButton = Button(inputSection, text='Quit', command=quit).grid(row=6,column=1)
 
 #Select Unit of temperature
 tUnits = ['K','C','F']
@@ -106,5 +117,6 @@ PressureEntry.grid(row=2,column=1)
 errorLabel = Label(inputSection, text='')
 errorLabel.grid(row=3,column=1)
 
-getConstants = Button(inputSection, text='Calculate', command= universal_constants).grid(row=0,column=2)
+getConstants = Button(inputSection, text='Calculate', command=universal_constants).grid(row=0,column=2)
+
 window.mainloop()
